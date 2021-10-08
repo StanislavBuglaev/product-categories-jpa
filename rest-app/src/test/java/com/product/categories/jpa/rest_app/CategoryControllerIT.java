cda592cc24b1187b6ddb1072d8e5be128447ee8c
StanislavBuglaev <StanislavBuglaev1991@gmail.com>
2021-10-08 15:24:28 -0700

    added rest controller tests (without update)

---
+++ rest-app/src/test/java/com/product/categories/jpa/rest_app/CategoryControllerIT.java
@@ -0,0 +1,152 @@
+package com.product.categories.jpa.rest_app;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.product.categories.jpa.entity.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@Transactional
+class CategoryControllerIT {
+
+    public static final String CATEGORIES_ENDPOINT = "/categories";
+
+    @Autowired
+    private CategoryController categoryController;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected MockMvc mockMvc;
+
+    protected MockCategoryService categoryService = new MockCategoryService();
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = standaloneSetup(categoryController)
+                .setMessageConverters(new MappingJackson2HttpMessageConverter())
+                .alwaysDo(MockMvcResultHandlers.print())
+                .build();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void categories() throws Exception {
+        List<Category> categories = categoryService.findAll();
+        assertNotNull(categories);
+        assertTrue(true);
+    }
+
+    @Test
+    void findById() throws Exception {
+        List<Category> categories = categoryService.findAll();
+        Assertions.assertNotNull(categories);
+        assertTrue(true);
+
+        Integer categoryId = categories.get(0).getCategoryId();
+        Category expCategory = categoryService.findById(categoryId).get();
+        Assertions.assertEquals(categoryId, expCategory.getCategoryId());
+        Assertions.assertEquals(categories.get(0).getCategoryName(), expCategory.getCategoryName());
+        Assertions.assertEquals(categories.get(0), expCategory);
+    }
+
+    @Test
+    void createCategory() throws Exception {
+       Category categoryTest =  categoryService.create(new Category());
+       assertTrue(categoryTest.getCategoryId() == 1);
+    }
+
+    @Test
+    void updateCategory() {
+    }
+
+    @Test
+    void deleteCategory() throws Exception {
+        Category categoryTest = categoryService.create(new Category());
+        assertTrue(categoryTest.getCategoryId() == 1);
+        categoryService.delete(categoryTest.getCategoryId());
+
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    private class MockCategoryService {
+
+        public List<Category> findAll() throws Exception {
+            MockHttpServletResponse response = mockMvc.perform(get(CATEGORIES_ENDPOINT)
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+                    .andReturn().getResponse();
+            assertNotNull(response);
+
+            return objectMapper.readValue(response.getContentAsString(), new TypeReference<List<Category>>() {});
+        }
+
+        public Optional<Category> findById(Integer categoryId) throws Exception {
+            MockHttpServletResponse response = mockMvc.perform(get(CATEGORIES_ENDPOINT + "/" + categoryId)
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+                    .andReturn().getResponse();
+            return Optional.of(objectMapper.readValue(response.getContentAsString(), Category.class));
+        }
+
+        public Category create(Category category) throws Exception {
+            String json = objectMapper.writeValueAsString(category);
+            MockHttpServletResponse response =
+                    mockMvc.perform(post(CATEGORIES_ENDPOINT)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(json)
+                            .accept(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isCreated())
+                            .andReturn().getResponse();
+            return objectMapper.readValue(response.getContentAsString(), Category.class);
+        }
+
+        public Category update(Category category) throws Exception {
+            MockHttpServletResponse response =
+                    mockMvc.perform(put(CATEGORIES_ENDPOINT)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(category))
+                            .accept(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isOk())
+                            .andReturn().getResponse();
+            return objectMapper.readValue(response.getContentAsString(), Category.class);
+        }
+
+        public void delete(Integer categoryId) throws Exception {
+            MockHttpServletResponse response = mockMvc.perform(
+                    MockMvcRequestBuilders.delete(new StringBuilder(CATEGORIES_ENDPOINT).append("/")
+                            .append(categoryId).toString())
+                            .accept(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+                    .andReturn().getResponse();
+        }
+    }
+}
---
+++ rest-app/src/test/java/com/product/categories/jpa/rest_app/ProductControllerIT.java
@@ -0,0 +1,152 @@
+package com.product.categories.jpa.rest_app;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.product.categories.jpa.entity.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIT {
+
+    public static final String PRODUCTS_ENDPOINT = "/products";
+
+    @Autowired
+    private ProductController productController;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected MockMvc mockMvc;
+
+    protected ProductControllerIT.MockProductService productService = new ProductControllerIT.MockProductService();
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = standaloneSetup(productController)
+                .setMessageConverters(new MappingJackson2HttpMessageConverter())
+                .alwaysDo(MockMvcResultHandlers.print())
+                .build();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void products() throws Exception {
+        List<Product> products = productService.findAll();
+        assertNotNull(products);
+        assertTrue(true);
+    }
+
+    @Test
+    void findById() throws Exception {
+        List<Product> products = productService.findAll();
+        Assertions.assertNotNull(products);
+        assertTrue(true);
+
+        Integer productId = products.get(0).getProductId();
+        Product expProduct = productService.findById(productId).get();
+        Assertions.assertEquals(productId, expProduct.getProductId());
+        Assertions.assertEquals(products.get(0).getProductName(), expProduct.getProductName());
+        Assertions.assertEquals(products.get(0), expProduct);
+    }
+
+    @Test
+    void createProduct() throws Exception {
+        Product productTest =  productService.create(new Product());
+        assertTrue(productTest.getProductId() == 1);
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void deleteProduct() throws Exception {
+        Product productTest = productService.create(new Product());
+        assertTrue(productTest.getProductId() == 1);
+        productService.delete(productTest.getProductId());
+
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    private class MockProductService {
+
+        public List<Product> findAll() throws Exception {
+            MockHttpServletResponse response = mockMvc.perform(get(PRODUCTS_ENDPOINT)
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+                    .andReturn().getResponse();
+            assertNotNull(response);
+
+            return objectMapper.readValue(response.getContentAsString(), new TypeReference<List<Product>>() {});
+        }
+
+        public Optional<Product> findById(Integer productId) throws Exception {
+            MockHttpServletResponse response = mockMvc.perform(get(PRODUCTS_ENDPOINT + "/" + productId)
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+                    .andReturn().getResponse();
+            return Optional.of(objectMapper.readValue(response.getContentAsString(), Product.class));
+        }
+
+        public Product create(Product product) throws Exception {
+            String json = objectMapper.writeValueAsString(product);
+            MockHttpServletResponse response =
+                    mockMvc.perform(post(PRODUCTS_ENDPOINT)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(json)
+                            .accept(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isCreated())
+                            .andReturn().getResponse();
+            return objectMapper.readValue(response.getContentAsString(), Product.class);
+        }
+
+        public Product update(Product product) throws Exception {
+            MockHttpServletResponse response =
+                    mockMvc.perform(put(PRODUCTS_ENDPOINT)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(product))
+                            .accept(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isOk())
+                            .andReturn().getResponse();
+            return objectMapper.readValue(response.getContentAsString(), Product.class);
+        }
+
+        public void delete(Integer productId) throws Exception {
+            MockHttpServletResponse response = mockMvc.perform(
+                    MockMvcRequestBuilders.delete(new StringBuilder(PRODUCTS_ENDPOINT).append("/")
+                            .append(productId).toString())
+                            .accept(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+                    .andReturn().getResponse();
+        }
+    }
+}
